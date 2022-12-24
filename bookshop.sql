920c6d34b06ee403ea62cb1c93155ead0e4dfceb
aleLovadina <alelova03@gmail.com>
2022-12-24 18:11:14 -0500

    updated database

--- bookshop.sql
+++ bookshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2022 at 04:27 AM
+-- Generation Time: Dec 25, 2022 at 12:07 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -24,74 +24,96 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dettaglio_ordine`
+-- Table structure for table `orders`
 --
 
-CREATE TABLE `dettaglio_ordine` (
-  `nordine` int(11) NOT NULL,
-  `idprodotto` int(11) NOT NULL,
-  `ncopie` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ordine`
---
-
-CREATE TABLE `ordine` (
-  `nordine` int(11) NOT NULL,
-  `data_ora` datetime NOT NULL DEFAULT current_timestamp(),
+CREATE TABLE `orders` (
+  `orderNum` int(11) NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp(),
   `username` varchar(50) NOT NULL,
-  `stato` varchar(20) NOT NULL DEFAULT 'inserito'
+  `status` varchar(20) NOT NULL DEFAULT 'saved'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`orderNum`, `date_time`, `username`, `status`) VALUES
+(24, '2022-12-24 17:46:08', 'mbertuola', 'saved'),
+(25, '2022-12-24 17:46:21', 'mbertuola', 'saved'),
+(26, '2022-12-24 17:47:03', 'mbertuola', 'saved'),
+(29, '2022-12-24 17:53:32', 'mbertuola', 'saved'),
+(30, '2022-12-24 17:54:10', 'mbertuola', 'saved');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `prodotto`
+-- Table structure for table `order_details`
 --
 
-CREATE TABLE `prodotto` (
-  `idProdotto` int(11) NOT NULL,
-  `titolo` varchar(50) NOT NULL,
-  `autore` varchar(50) DEFAULT NULL,
-  `editore` varchar(50) DEFAULT NULL,
-  `prezzo` decimal(10,2) DEFAULT 0.00,
-  `fotografia` varchar(50) NOT NULL,
-  `giacenza` int(11) DEFAULT 0
+CREATE TABLE `order_details` (
+  `orderNum` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `copiesNum` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `prodotto`
+-- Dumping data for table `order_details`
 --
 
-INSERT INTO `prodotto` (`idProdotto`, `titolo`, `autore`, `editore`, `prezzo`, `fotografia`, `giacenza`) VALUES
-(1, 'Corso di informatica', 'Formichi', 'Zanichelli', '29.90', '../homepage/Tulips.jpg', 123),
-(2, 'Linguaggio Python', 'Lorenzi', 'Atlas', '25.00', '../homepage/Penguins.jpg', 123),
+INSERT INTO `order_details` (`orderNum`, `productId`, `copiesNum`) VALUES
+(24, 2, 2),
+(26, 1, 1),
+(29, 1, 1),
+(30, 1, 1),
+(30, 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `productId` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `publisher` varchar(50) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT 0.00,
+  `cover` varchar(50) NOT NULL,
+  `stock` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`productId`, `title`, `author`, `publisher`, `price`, `cover`, `stock`) VALUES
+(1, 'Corso di informatica', 'Formichi', 'Zanichelli', '29.90', '../homepage/Tulips.jpg', 115),
+(2, 'Linguaggio Python', 'Lorenzi', 'Atlas', '25.00', '../homepage/Penguins.jpg', 114),
 (11, 'Avventure 2', NULL, NULL, '40.00', '../homepage/Desert.jpg', 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utente`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `utente` (
+CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `nome` varchar(50) DEFAULT NULL,
-  `cognome` varchar(50) DEFAULT NULL,
-  `indirizzo` varchar(100) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
-  `ruolo` varchar(10) NOT NULL DEFAULT '"user"'
+  `role` varchar(10) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `utente`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `utente` (`username`, `password`, `nome`, `cognome`, `indirizzo`, `email`, `ruolo`) VALUES
+INSERT INTO `users` (`username`, `password`, `name`, `last_name`, `address`, `email`, `role`) VALUES
 ('admin', 'admin', NULL, NULL, NULL, NULL, 'admin'),
 ('mberton', 'mberton', NULL, NULL, NULL, NULL, 'user'),
 ('mbertuola', 'mbertuola', NULL, NULL, NULL, NULL, 'user');
@@ -101,29 +123,29 @@ INSERT INTO `utente` (`username`, `password`, `nome`, `cognome`, `indirizzo`, `e
 --
 
 --
--- Indexes for table `dettaglio_ordine`
+-- Indexes for table `orders`
 --
-ALTER TABLE `dettaglio_ordine`
-  ADD PRIMARY KEY (`nordine`,`idprodotto`),
-  ADD KEY `idprodotto` (`idprodotto`);
-
---
--- Indexes for table `ordine`
---
-ALTER TABLE `ordine`
-  ADD PRIMARY KEY (`nordine`),
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`orderNum`),
   ADD KEY `username` (`username`);
 
 --
--- Indexes for table `prodotto`
+-- Indexes for table `order_details`
 --
-ALTER TABLE `prodotto`
-  ADD PRIMARY KEY (`idProdotto`);
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`orderNum`,`productId`),
+  ADD KEY `idprodotto` (`productId`);
 
 --
--- Indexes for table `utente`
+-- Indexes for table `products`
 --
-ALTER TABLE `utente`
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productId`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
   ADD PRIMARY KEY (`username`),
   ADD UNIQUE KEY `email` (`email`);
 
@@ -132,27 +154,27 @@ ALTER TABLE `utente`
 --
 
 --
--- AUTO_INCREMENT for table `ordine`
+-- AUTO_INCREMENT for table `orders`
 --
-ALTER TABLE `ordine`
-  MODIFY `nordine` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders`
+  MODIFY `orderNum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `dettaglio_ordine`
+-- Constraints for table `orders`
 --
-ALTER TABLE `dettaglio_ordine`
-  ADD CONSTRAINT `dettaglio_ordine_ibfk_1` FOREIGN KEY (`idprodotto`) REFERENCES `prodotto` (`idProdotto`),
-  ADD CONSTRAINT `dettaglio_ordine_ibfk_2` FOREIGN KEY (`nordine`) REFERENCES `ordine` (`nordine`);
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
 
 --
--- Constraints for table `ordine`
+-- Constraints for table `order_details`
 --
-ALTER TABLE `ordine`
-  ADD CONSTRAINT `ordine_ibfk_1` FOREIGN KEY (`username`) REFERENCES `utente` (`username`);
+ALTER TABLE `order_details`
+  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`ProductID`),
+  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`orderNum`) REFERENCES `orders` (`orderNum`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
